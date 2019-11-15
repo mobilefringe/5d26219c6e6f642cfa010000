af691c55dd637afede2d411a5120cea80aa4e9b0
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-15 14:00:23 -0500

    The Marketplace at Calimesa 1.0  /sales_and_events.vue

--- sales_and_events.vue
+++ sales_and_events.vue
@@ -64,8 +64,8 @@
                                 <b-card-header header-tag="header" class="p-1" role="tab">
                                     <b-btn block @click="togglePromos = !togglePromos" :aria-expanded="togglePromos ? 'true' : 'false'" aria-controls="togglePromotions">
                                         Sales
-                                        <i v-if="togglePromos"  class="fa fa-minus f"></i>
-                                        <i v-else  class="fa fa-plus"></i>
+                                        <!--<i v-if="togglePromos"  class="fa fa-minus f"></i>-->
+                                        <!--<i v-else  class="fa fa-plus"></i>-->
                                     </b-btn>
                                 </b-card-header>
                                 
